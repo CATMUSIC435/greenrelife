8e14144dd06a3bf9922f27ef66205f996709e531
DongIOT <94212576+TRINHDINHDAI2306@users.noreply.github.com>
2025-11-20 22:17:38 +0700

    update: ui

--- src/components/molecules/icon-menu.tsx
+++ src/components/molecules/icon-menu.tsx
@@ -1,14 +1,15 @@
 'use client';
 import { Bell, HelpCircle, Home, Settings, Shield } from 'lucide-react';
+import { redirect } from 'next/navigation';
 import React from 'react';
 import { cn } from '@/lib/utils';
 
 const items = [
-  { id: 'home', icon: Home },
-  { id: 'bell', icon: Bell },
-  { id: 'settings', icon: Settings },
-  { id: 'help', icon: HelpCircle },
-  { id: 'shield', icon: Shield },
+  { id: 'home', icon: Home, link: '/' },
+  { id: 'bell', icon: Bell, link: '/chat' },
+  { id: 'settings', icon: Settings, link: '/' },
+  { id: 'help', icon: HelpCircle, link: '/' },
+  { id: 'shield', icon: Shield, link: '/' },
 ];
 
 export default function IconMenu() {
@@ -16,11 +17,12 @@ export default function IconMenu() {
 
   return (
     <div className="flex w-max items-center gap-3 rounded-full border bg-white/40 px-2 shadow-sm backdrop-blur-md">
-      {items.map(({ id, icon: Icon }) => (
+      {items.map(({ id, icon: Icon, link }) => (
         <button
           key={id}
           onClick={() => {
             setActive(id);
+            redirect(link);
           }}
           className={cn(
             'p-3 rounded-full transition-all flex items-center justify-center',
