9ae72b3d76fc1fc05ec5e4a26700d19c5b3e5442
DongIOT <94212576+TRINHDINHDAI2306@users.noreply.github.com>
2025-11-21 17:01:06 +0700

    fix: ui

--- src/app/[locale]/(marketing)/blog/[slug]/page.tsx
+++ src/app/[locale]/(marketing)/blog/[slug]/page.tsx
@@ -20,7 +20,7 @@ export default async function Page({ params }: { params: Promise<{ locale: strin
   return (
     <div className="mx-auto max-w-3xl px-4 pt-2 pb-20">
       <h1 className="mb-4 text-center text-2xl font-bold">{post.title.rendered}</h1>
-      {post.yoast_head_json.og_image[0]?.url && <img src={post.yoast_head_json.og_image[0]?.url} alt={post.title} className="mb-6" />}
+      {post.yoast_head_json.og_image[0]?.url && <img src={post.yoast_head_json.og_image[0]?.url} alt={post.title.rendered} className="mb-6" />}
       <p className="mb-6 text-gray-500">{new Date(post.date).toLocaleDateString()}</p>
       <div dangerouslySetInnerHTML={{ __html: `${post.content.rendered}` }} />
     </div>
