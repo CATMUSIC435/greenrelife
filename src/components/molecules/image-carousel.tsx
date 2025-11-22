222c58a145f49bbe6a7325be9443bf16c884e2d2
DongIOT <94212576+TRINHDINHDAI2306@users.noreply.github.com>
2025-11-22 12:08:05 +0700

    fix: type

--- src/components/molecules/image-carousel.tsx
+++ src/components/molecules/image-carousel.tsx
@@ -3,8 +3,7 @@
 
 import Image from 'next/image';
 
-// eslint-disable-next-line unused-imports/no-unused-vars
-export default function ImageCarousel({ images = [], selectedIndex = 0, onChange = (i: number) => { } }: {
+export default function ImageCarousel({ images = [], selectedIndex = 0, onChange }: {
   images: any[];
   selectedIndex?: number;
   onChange?: (i: number) => void;
@@ -30,7 +29,7 @@ export default function ImageCarousel({ images = [], selectedIndex = 0, onChange
       {/* simple controls */}
       <div className="mt-2 flex items-center justify-between">
         <button
-          onClick={() => onChange(Math.max(0, selectedIndex - 1))}
+          onClick={() => onChange && onChange(Math.max(0, selectedIndex - 1))}
           className="rounded border px-3 py-1 disabled:opacity-50"
           disabled={selectedIndex === 0}
           type="button"
@@ -47,7 +46,7 @@ export default function ImageCarousel({ images = [], selectedIndex = 0, onChange
         </div>
 
         <button
-          onClick={() => onChange(Math.min(images.length - 1, selectedIndex + 1))}
+          onClick={() => onChange && onChange(Math.min(images.length - 1, selectedIndex + 1))}
           className="rounded border px-3 py-1 disabled:opacity-50"
           disabled={selectedIndex === images.length - 1}
           type="button"
